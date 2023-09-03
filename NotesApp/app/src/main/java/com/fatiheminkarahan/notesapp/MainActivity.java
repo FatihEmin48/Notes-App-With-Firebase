805dd8731f38853f2d7e6cfd6444cd6aa7513e29
Fatih Emin KARAHAN <fatih81443@gmail.com>
2023-09-03 11:39:36 +0300

    Firebase Add Note

--- NotesApp/app/src/main/java/com/fatiheminkarahan/notesapp/MainActivity.java
+++ NotesApp/app/src/main/java/com/fatiheminkarahan/notesapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.fatiheminkarahan.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addNoteBtn = findViewById(R.id.add_note_btn);
+        recyclerView = findViewById(R.id.recyler_view);
+        menuBtn = findViewById(R.id.menu_btn);
+
+        addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this, NoteDetailsActivity.class)) );
+
     }
 }
